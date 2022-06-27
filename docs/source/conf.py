1ba3f240ba2ce8581fc6388e59730b5be244409c
Alexander J. Gabourie <agabourie47@gmail.com>
2022-06-26 21:47:44 -0700

    All classes get their __init__ docstrings shown

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -49,6 +49,7 @@ exclude_patterns = []
 #
 html_theme = 'sphinx_rtd_theme'
 html_style = 'gpyumd.css'
+autoclass_content = 'both'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
